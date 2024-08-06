652dd6899cfc415253eefb304b9c2644e9c396be
kimaita <johnnieqaym@gmail.com>
2024-08-06 17:53:18 +0300

    added task #8

---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,18 @@
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const incomeYear = `income-${getCurrentYear()}`;
+  const gdpYear = `gdp-${getCurrentYear()}`;
+  const capitaYear = `capita-${getCurrentYear()}`;
+
+  const budget = {
+    [incomeYear]: income,
+    [gdpYear]: gdp,
+    [capitaYear]: capita,
+  };
+
+  return budget;
+}
