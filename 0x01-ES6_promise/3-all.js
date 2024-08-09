cb9276dc3b7800cb13f4c59cc48fabd846a0b9b6
kimaita <johnnieqaym@gmail.com>
2024-08-09 19:51:06 +0300

    added task #3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+import { createUser, uploadPhoto } from './utils'
+
+export default function handleProfileSignup() {
+  Promise.all([createUser(), uploadPhoto()])
+    .then(
+      (results) => {
+        const names = results[0];
+        const photo = results[1];
+        console.log(`${photo.body} ${names.firstName} ${names.lastName}`);
+      },
+      () => console.log('Signup system offline'),
+    );
+}
