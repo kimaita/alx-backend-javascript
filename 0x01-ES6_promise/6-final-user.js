b303944ce1ecb31622708109e315ed6d5fd015b2
kimaita <johnnieqaym@gmail.com>
2024-08-09 23:39:01 +0300

    promises, right?

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,10 +1,15 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(
+  firstName,
+  lastName,
+  fileName,
+) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-  return Promise.allSettled(promises).then((results) => Array.from(results, (res) => ({
-    status: res.status,
-    value: res.value || res.reason,
-  })));
+  return Promise.allSettled(promises).then((results) => (
+    Array.from(results, (res) => ({
+      status: res.status,
+      value: res.value || res.reason,
+    }))));
 }
