cad6ad137bc0268c479ad64a4499d16081953077
kimaita <johnnieqaym@gmail.com>
2024-10-01 12:01:05 +0300

    use readline.line

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -7,9 +7,12 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+console.log('Welcome to Holberton School, what is your name?');
+
+rl.on('line', (answer) => {
+  console.log(`Your name is: ${answer}`);
+  rl.pause();
+});
 rl.on('close', () => {
   console.log('This important software is now closing');
-});
-rl.question('Welcome to Holberton School, what is your name? ', (answer) => {
-  console.log(`Your name is: ${answer}`);
 });
