13a6a60f7911fc8a965a4e62cdeadbe0935ad6b7
kimaita <johnnieqaym@gmail.com>
2024-10-02 03:55:28 +0300

    refactoring

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,12 +3,16 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 1245;
 
-const app = http.createServer((req, res) => {
+const requestListener = (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!\n');
-});
+};
 
-app.listen(port, hostname);
+const app = http.createServer(requestListener);
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
 
 module.exports = app;
