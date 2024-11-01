954805934d25c86838de173f654ac9979cc52299
kimaita <johnnieqaym@gmail.com>
2024-11-01 11:20:32 +0300

    remove readline module

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,12 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
 process.stdout.write('Welcome to Holberton School, what is your name?');
 
-rl.on('line', (answer) => {
-  process.stdout.write(`Your name is: ${answer}`);
-  rl.pause();
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
-rl.on('close', () => {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing');
 });
