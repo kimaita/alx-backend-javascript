4134bf71b391fce2680ecb998aef6d6ce248ba30
kimaita <johnnieqaym@gmail.com>
2024-11-01 11:18:06 +0300

    remove console.log

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,15 @@
 const readline = require('readline');
 
-// eslint-disable-next-line no-unused-vars
-const { stdin: input, stdout: output } = require('child_process');
-
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?');
 
 rl.on('line', (answer) => {
-  console.log(`Your name is: ${answer}`);
+  process.stdout.write(`Your name is: ${answer}`);
   rl.pause();
 });
 rl.on('close', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing');
 });
