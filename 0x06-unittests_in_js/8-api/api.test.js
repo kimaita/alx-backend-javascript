4a6a2e44cd2b91654bff2a553003f2e3695cb4fa
kimaita <johnnieqaym@gmail.com>
2024-11-04 09:59:08 +0300

    rewrite

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,35 +1,14 @@
-const request = require('supertest');
+const request = require('request');
 const { expect } = require('chai');
-const app = require('./api');
 
-describe('API Tests', () => {
-  describe('Index Page', () => {const request = require('request');
-    const { expect } = require('chai');
-    
-    describe('API integration test', () => {
-      const API_URL = 'http://localhost:7865';
-    
-      it('GET / returns correct response', (done) => {
-        request.get(`${API_URL}/`, (_err, res, body) => {
-          expect(res.statusCode).to.be.equal(200);
-          expect(body).to.be.equal('Welcome to the payment system');
-          done();
-        });
-      });
-    });
-    it('Should return status code 200', async () => {
-      const res = await request(app).get('/');
-      expect(res.status).to.equal(200);
-    });
+describe('API integration test', () => {
+  const API_URL = 'http://localhost:7865';
 
-    it('Should return the message "Welcome to the payment system"', async () => {
-      const res = await request(app).get('/');
-      expect(res.text).to.equal('Welcome to the payment system');
-    });
-
-    it('Should not return an incorrect message', async () => {
-      const res = await request(app).get('/');
-      expect(res.text).to.not.equal('Incorrect message');
+  it('GET / returns correct response', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
     });
   });
 });
